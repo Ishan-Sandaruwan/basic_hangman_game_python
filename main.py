8b82d6ee8f2fbf21bb26856c28899d388c8b2cb1
W I Sandaruwan <138509167+Ishan-Sandaruwan@users.noreply.github.com>
2023-11-01 20:59:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
-#Step 2
+#Step 5
 
 import random
-word_list = ["aardvark", "baboon", "camel"]
+
+from hangman_words import word_list
+
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-gameList = []
-for char in chosen_word:
-  gameList.append("_")
-print(gameList)
+end_of_game = False
+lives = 6
 
-guess = input("Guess a letter: ").lower()
+from hangman_art import logo
+print(logo)
 
-#Loop through each position in the chosen_word;
-for letter in range(0,len(chosen_word)):
-    if chosen_word[letter] == guess:
-      gameList[letter] = guess
+display = []
+for _ in range(word_length):
+    display += "_"
 
-print(gameList)
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-#Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    from hangman_art import stages
+    print(stages[lives])
